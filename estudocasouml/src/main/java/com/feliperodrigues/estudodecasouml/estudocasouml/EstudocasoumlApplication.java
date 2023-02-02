f5c9a0b35dee538070045085589481120bbb784c
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-02-02 06:31:08 -0300

    First commit

---
+++ estudocasouml/src/main/java/com/feliperodrigues/estudodecasouml/estudocasouml/EstudocasoumlApplication.java
@@ -0,0 +1,13 @@
+package com.feliperodrigues.estudodecasouml.estudocasouml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudocasoumlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudocasoumlApplication.class, args);
+	}
+
+}
---
+++ estudocasouml/src/test/java/com/feliperodrigues/estudodecasouml/estudocasouml/EstudocasoumlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feliperodrigues.estudodecasouml.estudocasouml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudocasoumlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
