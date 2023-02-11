835d0cd6af5c35abd0a894e402349a2e0276d308
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-02-11 13:56:11 -0300

    Retornando a autorização com STRING VIA HTML

--- estudocasouml/src/main/java/com/feliperodrigues/estudodecasouml/estudocasouml/SecurityConfig.java
+++ estudocasouml/src/main/java/com/feliperodrigues/estudodecasouml/estudocasouml/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.feliperodrigues.estudodecasouml.estudocasouml;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Profile(value = "security")
 public class SecurityConfig {
 
     @Bean
@@ -22,7 +24,7 @@ public class SecurityConfig {
 
                     authorizeConfig.anyRequest().authenticated();
                 }
-        ).formLogin(Customizer.withDefaults())
+        ).oauth2Login(Customizer.withDefaults())
         .build();
     }
 
--- estudocasouml/src/main/java/com/feliperodrigues/estudodecasouml/estudocasouml/resources/EstadoResource.java
+++ estudocasouml/src/main/java/com/feliperodrigues/estudodecasouml/estudocasouml/resources/EstadoResource.java
@@ -5,30 +5,38 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.*;
 
 import com.feliperodrigues.estudodecasouml.estudocasouml.entities.Estado;
 import com.feliperodrigues.estudodecasouml.estudocasouml.services.EstadoService;
 
+
 @RestController
-@RequestMapping(value = "/estados")
 public class EstadoResource {
 
 	@Autowired
 	private EstadoService estadoService;
-	
-	@GetMapping
+
+	/*@RequestMapping(value = "/estados", method = RequestMethod.GET)
 	public ResponseEntity<List<Estado>> findAll(){
 		List<Estado> obj = estadoService.findAll();
 		return ResponseEntity.ok().body(obj);
-	}
-	
-	@GetMapping(value = "/{id}")
+	}*/
+
+	@RequestMapping(value = "/estados/{id}", method = RequestMethod.GET)
     public ResponseEntity<Optional<Estado>> findById(@PathVariable Integer id){
 		Estado obj = estadoService.findById(id);
         return ResponseEntity.ok().body(Optional.ofNullable(obj));
     }
+
+	@RequestMapping(value = "/estados", method = RequestMethod.GET)
+	public String privateRoute(@AuthenticationPrincipal OidcUser principal){
+		return String.format("<h1>Private route, only authorized personal! </h1>" +
+				"<h3>Principal: %s </h3>" +
+				"<h3>Email attribute: %s </h3>" +
+				"<h3>Authorities: %s </h3>" +
+				"<h3>JWT: %s</h3>", principal, principal.getAttributes(), principal.getAuthorities(), principal.getIdToken().getTokenValue());
+	}
 }
